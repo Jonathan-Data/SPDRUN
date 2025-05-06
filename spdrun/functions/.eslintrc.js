0675035a911c1f4ffe950d484987263639a1fba9
jolauwer <Jnthnlauwers@gmail.com>
2025-05-06 13:56:36 +0200

    added firebase integration

--- spdrun/functions/.eslintrc.js
+++ spdrun/functions/.eslintrc.js
@@ -1,1 +1,24 @@
-module.exports = { extends: ['eslint:recommended'] };
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files
+  ],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "quotes": ["error", "double"],
+    "indent": ["error", 2]
+  },
+};
